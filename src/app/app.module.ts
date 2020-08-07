387d073ecc481c1dd904f6c6de857ce9ac409e72
Peter v. Niebelschütz <fogshot@gmail.com>
2020-08-07 18:34:47 +0200

    reorganize components; add ship-details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,19 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {UserDetailsComponent} from './user-details/user-details.component';
+import {UserDetailsComponent} from './users/user-details/user-details.component';
 import {RouterModule} from '@angular/router';
-import {ShipListComponent} from './ship-list/ship-list.component';
+import {ShipListComponent} from './ships/ship-list/ship-list.component';
 import {MatTableModule} from '@angular/material/table';
 import {HttpClientModule} from '@angular/common/http';
+import { ShipDetailsComponent } from './ships/ship-details/ship-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserDetailsComponent,
     ShipListComponent,
+    ShipDetailsComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/ships/ship.service.ts
+++ src/app/ships/ship.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {Ship} from './ship';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {environment} from '../environments/environment';
+import {environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
